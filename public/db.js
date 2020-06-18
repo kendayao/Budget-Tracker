089732b7e664ae5803c7e0c6da0238575a589da5
Kaeneth Dayao <kendayao@gmail.com>
2020-06-17 17:58:53 -0700

    code refactoring

--- public/db.js
+++ public/db.js
@@ -1,7 +1,9 @@
 let db;
 
+// create budget database
 const request = window.indexedDB.open("budget", 1)
 
+// create "pending" object store
 request.onupgradeneeded = function(event) {
    const db = event.target.result;
    db.createObjectStore("pending", { autoIncrement: true });
@@ -19,13 +21,14 @@ request.onupgradeneeded = function(event) {
     console.log("Woops! " + event.target.errorCode);
   };
 
-  
+  // create transaction on pending object store to add record to store
   function saveRecord(record) {
     const transaction = db.transaction(["pending"], "readwrite");
     const pendingStore = transaction.objectStore("pending");
     pendingStore.add(record);
   }
 
+  // create transaction on pending object store to get all records from store
   function checkDatabase() {
     const transaction = db.transaction(["pending"], "readwrite");
     const pendingStore = transaction.objectStore("pending");
@@ -44,7 +47,7 @@ request.onupgradeneeded = function(event) {
           })
           .then(response => response.json())
           .then(() => {
-            
+            // create transaction on pending object store to clear all records from store
             const transaction = db.transaction(["pending"], "readwrite");
             const pendingStore = transaction.objectStore("pending");
             pendingStore.clear();
